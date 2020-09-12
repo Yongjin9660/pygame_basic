bf326998766d64e7bef7d9d972afcc4bf55da15f
YongjinKim <dydwls0669@gmail.com>
2020-09-12 14:42:19 +0900

    character

--- 3_main_sprite.py
+++ 3_main_sprite.py
@@ -13,6 +13,14 @@ pygame.display.set_caption("Yongjin Game")     #게임 이름
 # 배경이미지 불러오기
 backgruond = pygame.image.load("C:/Users/Yongjin/pygame_basic/IMAGE/background.png")
 
+# 캐릭터(스프라이트) 불러오기
+character = pygame.image.load("C:/Users/Yongjin/pygame_basic/IMAGE/character.png")
+character_size = character.get_rect().size      # 이미지의 크기를 구해옴
+character_width = character_size[0]         # 캐릭터의 가로 크기
+character_height = character_size[1]        # 캐릭터의 세로 크기
+character_x_pos = screen_width / 2 - character_width / 2    # 화면 가로의 절반 크기에 해당하는 곳에 위치
+character_y_pos = screen_height - character_height          # 화면 세로 크기 가장 아래에 해당하는 곳에 위치
+
 # 이벤트 루프
 running = True      # 게임이 진행중인가?
 while running:
@@ -21,6 +29,8 @@ while running:
             running = False             # 게임이 진행중이 아님
     
     screen.blit(backgruond, (0,0))      # 배경 그리기
+
+    screen.blit(character, (character_x_pos, character_y_pos))  # 캐릭터그리기
 
     pygame.display.update()             # 게임화면을 다시 그리기!
 
