4ba0fe48224d1d743e3430e89bb1c38ca5e8cbe7
YongjinKim <dydwls0669@gmail.com>
2020-09-12 15:26:59 +0900

    FPS

--- 4_keyboard_event.py
+++ 4_keyboard_event.py
@@ -10,6 +10,9 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 # 화면 타이틀 설정
 pygame.display.set_caption("Yongjin Game")     #게임 이름
 
+# FPS
+clock = pygame.time.Clock()
+
 # 배경이미지 불러오기
 backgruond = pygame.image.load("C:/Users/Yongjin/pygame_basic/IMAGE/background.png")
 
@@ -28,6 +31,7 @@ to_y = 0
 # 이벤트 루프
 running = True      # 게임이 진행중인가?
 while running:
+    dt = clock.tick(30)     # 게임화면의 초당 프레임 수를 설정
     for event in pygame.event.get():    # 어떤 이벤트가 발생하였는가
         if event.type == pygame.QUIT:   # 창이 닫히는 이벤트가 발생
             running = False             # 게임이 진행중이 아님
